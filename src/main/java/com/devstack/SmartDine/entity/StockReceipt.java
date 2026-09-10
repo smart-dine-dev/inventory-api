e6122711923467e9899031c3bc26c8fee5842365
education-bootcamp <education.seekerscloud@gmail.com>
2026-09-10 21:38:17 +0530

    kinit

--- src/main/java/com/devstack/SmartDine/entity/StockReceipt.java
+++ src/main/java/com/devstack/SmartDine/entity/StockReceipt.java
@@ -2,8 +2,7 @@ package com.devstack.SmartDine.entity;
 
 
 import com.devstack.SmartDine.entity.enums.ReceiptStatus;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -21,15 +20,25 @@ import java.util.UUID;
 @NoArgsConstructor
 @Builder
 public class StockReceipt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    @Column(nullable = false, unique = true, length = 18)
     private String receiptNumber;
+    @Column(nullable = false)
     private LocalDate receivedDate;
+    @Column(precision = 10, scale = 2)
     private BigDecimal totalAmount;
+    @Column(length = 500)
     private String notes;
+    @Column(length = 100)
     private String receivedBy;
     @CreationTimestamp
     private LocalDateTime createdAt;
     @UpdateTimestamp
     private LocalDateTime updatedAt;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
     private ReceiptStatus status = ReceiptStatus.PENDING;
 }
