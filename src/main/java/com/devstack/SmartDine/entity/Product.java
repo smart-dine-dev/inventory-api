aa91aeed2e8e555307a87fa0b1fa02632bf4efd5
education-bootcamp <education.seekerscloud@gmail.com>
2026-09-10 21:14:54 +0530

    kinit

--- src/main/java/com/devstack/SmartDine/entity/Product.java
+++ src/main/java/com/devstack/SmartDine/entity/Product.java
@@ -1,9 +1,10 @@
 package com.devstack.SmartDine.entity;
 
 import com.devstack.SmartDine.entity.enums.ProductStatus;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -17,21 +18,47 @@ import java.util.UUID;
 @NoArgsConstructor
 @Builder
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    @Column(nullable = false, unique = true, length = 50)
     private String SKU;
+    @Column(nullable = false, length = 150)
     private String name;
+    @Column(length = 500)
     private String description;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
     private Category category;
+    @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal unitPrice;
+    @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal costPrice;
+    @Column(length = 20)
     private String unit;
+    @Column(nullable = false)
+    @Builder.Default
     private Integer currentStock=0;
+
+    @Column(nullable = false)
+    @Builder.Default
     private Integer minimumStock=20;
+
+    @Column(nullable = false)
+    @Builder.Default
     private Integer maximumStock=500;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
     private ProductStatus status = ProductStatus.ACTIVE;
+    @Column(nullable = false)
+    @Builder.Default
     private Boolean availableForOrder;
     private String imageUrl;
+    @CreationTimestamp
     private LocalDateTime createdAt;
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 
 }
